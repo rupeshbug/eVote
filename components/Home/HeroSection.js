73ab3ef66eb35183286544099a5fba73a3cb39ea
rupeshbug <chaulagainrupesh1@gmail.com>
2023-05-23 18:02:36 +0545

    changes made to Navbar and HeroSection

--- components/Home/HeroSection.js
+++ components/Home/HeroSection.js
@@ -3,20 +3,28 @@ import backgroundImage from "../../public/background.webp";
 const HeroSection = () => {
   return (
     <div
-      className="bg-cover bg-center h-screen flex text-white"
+      className="bg-cover bg-center flex flex-col-reverse md:flex-row text-white"
       style={{ backgroundImage: `url(${backgroundImage.src})` }}
     >
-      <div className="w-1/2 mt-20 px-10">
-        <h1 className="text-5xl tracking-wide" style={{ lineHeight: "1.3" }}>
+      <div className="w-full md:w-1/2 mt-10 md:mt-20 px-10 order-2 md:order-1">
+        <h1
+          className="text-3xl md:text-5xl tracking-wide text-center md:text-left"
+          style={{ lineHeight: "1.5" }}
+        >
           Have a say in the decisions that impact your community.
         </h1>
-        <p className="py-5 tracking-wide text-lg">
+        <p className="py-5 tracking-wide text-lg text-center md:text-left">
           An online voting platform you can trust.
         </p>
+        <div className="text-center md:text-left mb-4">
+          <button className="bg-teal-600 hover:bg-teal-700 rounded-lg px-5 py-2 text-xl">
+            Register
+          </button>
+        </div>
       </div>
-      <div className="w-1/2 px-10">
+      <div className="w-full md:w-1/2 px-10 order-1 md:order-2">
         <img
-          className="h object-cover w-full opacity-75"
+          className="h object-cover w-full opacity-75 sm:height-200"
           src="/Interface.png"
           alt="interface"
         />
--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,15 +1,18 @@
-import React from "react";
-
 const Navbar = () => {
   return (
     <nav
-      className="h-20 px-10 bg-161635 flex justify-between items-center text-white border-b-2 border-slate"
+      className="h-20 px-10 bg-white shadow md:flex md:items-center md:justify-between"
       style={{ backgroundColor: "#161635" }}
     >
-      <div className="flex items-center px-4 text-xl font-bold w-1/2">
-        eVote
+      <div className="flex items-center text-xl font-bold w-1/2">
+        <img
+          className="cursor-pointer"
+          style={{ height: "64px" }}
+          src="/logo-no-bg.png"
+          alt="hero background"
+        />
       </div>
-      <ul className="flex justify-around w-1/2">
+      <ul className="flex justify-around w-1/2 text-white">
         <li>Home</li>
         <li>About Us</li>
         <li>Signup</li>
