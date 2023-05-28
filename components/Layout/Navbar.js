4c212f4a922e6f8b6395ce7b302fccf31cd70179
rupeshbug <chaulagainrupesh1@gmail.com>
2023-05-28 16:00:57 +0545

    redirect to timeline on signin

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -45,13 +45,9 @@ const Navbar = () => {
                   </div>
                 </Link>
               ) : (
-                <button
-                  onClick={() => signIn()}
-                  className="flex items-center justify-center max-w-xs px-4 py-3 rounded-lg border-2 w-60"
-                  style={{ color: "white" }}
-                >
-                  Sign In
-                </button>
+                <Link href="/login">
+                  <button style={{ color: "white" }}>Sign In</button>
+                </Link>
               )}
               {session ? (
                 <button
@@ -100,16 +96,9 @@ const Navbar = () => {
               </div>
             </Link>
           ) : (
-            <div className="mx-auto mt-5">
-              <button
-                onClick={() => signIn({})}
-                className="flex items-center justify-center max-w-xs px-4 py-3 rounded-lg border-2 w-60"
-                style={{ color: "white" }}
-              >
-                <FcGoogle className="w-5 h-5 mr-2" />
-                Sign In
-              </button>
-            </div>
+            <Link href="/login">
+              <button style={{ color: "white" }}>Sign In</button>
+            </Link>
           )}
           {session ? (
             <button
