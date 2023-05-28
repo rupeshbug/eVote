1b12e32347e02d512c91aba5cb5f36cacf8f40db
rupeshbug <chaulagainrupesh1@gmail.com>
2023-05-28 16:12:29 +0545

    create poll page design

--- pages/createPoll.js
+++ pages/createPoll.js
@@ -16,7 +16,60 @@ const CreatePoll = () => {
   return (
     <>
       <Navbar />
-      <h2>Create Poll</h2>
+      <h2 className="text-center text-4xl my-10">Create Poll</h2>
+      <div className="flex justify-center ">
+        <div class="w-full md:w-2/5">
+          <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="eventTitle"
+              >
+                Give Event Title
+              </label>
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="eventTitle"
+                type="text"
+                placeholder="Name of the event or topic"
+              />
+            </div>
+            <div class="mb-2">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="description"
+              >
+                Description
+              </label>
+              <textarea
+                rows={4}
+                class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="description"
+                type="text"
+                placeholder="Give description about event or topic"
+              />
+            </div>
+            <div class="mb-6">
+              <label class="block text-gray-700 text-sm font-bold mb-2">
+                Create Options
+              </label>
+              <input
+                class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Option 1"
+              />
+            </div>
+            <div class="flex items-center justify-between">
+              <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Create Event
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
